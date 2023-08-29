2f4a1643f3f0dce02497dfa345ceec738db92d7c
garuda-SSS <sipin1997@gmail.com>
2023-08-29 21:55:30 +0300

    Добавлены проверки, связанные с success message

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,33 +1,39 @@
 from .base_page import BasePage
-from .locators import BasketPageLocators
+from .locators import ProductPageLocators
 
 
-class BasketPage(BasePage):
+class ProductPage(BasePage):
     def put_to_basket(self):
-        basket=self.browser.find_element(*BasketPageLocators.BASKET_BUTTON)
+        basket=self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         basket.click()
         self.solve_quiz_and_get_code()
+        
+    def user_put_to_basket(self):
+        basket=self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
+        basket.click()
 
     def should_be_after_add(self):
         self.should_be_right_name()
         self.should_be_right_cost()
     
     def should_be_right_name(self):
-        name1=self.browser.find_element(*BasketPageLocators.NAME_IN_PAGE)
+        name1=self.browser.find_element(*ProductPageLocators.NAME_IN_PAGE)
         text1=name1.text
-        name2=self.browser.find_element(*BasketPageLocators.NAME_IN_BASKET)
+        name2=self.browser.find_element(*ProductPageLocators.NAME_IN_BASKET)
         text2=name2.text
-        
-        print(f"{text1} и {text2}")
-        
         assert text1==text2, "Wrong book is in basket"
 
     def should_be_right_cost(self):
-        price1=self.browser.find_element(*BasketPageLocators.PRICE_IN_PAGE)
+        price1=self.browser.find_element(*ProductPageLocators.PRICE_IN_PAGE)
         cost1=price1.text
-
-        price2=self.browser.find_element(*BasketPageLocators.PRICE_IN_BASKET)
+        price2=self.browser.find_element(*ProductPageLocators.PRICE_IN_BASKET)
         cost2=price1.text
-        
-        print(f"{cost1} и {cost2}")
         assert cost1==cost2, "Wrong price is in basket"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+       "Success message is presented, but should not be"
+
+    def should_be_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+       "Success message is presented, but should be hide"
