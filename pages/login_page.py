08a175fccb888425ce962a22a5fa0ff5d6de2d82
garuda-SSS <sipin1997@gmail.com>
2023-08-29 21:52:03 +0300

    Добавлены методы, связанные с регистрацией юзера

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,13 +9,22 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
         assert "login" in self.browser.current_url, "Register form is not presented"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
+
+    def register_new_user(self, email, password):
+        pochya=self.browser.find_element(*LoginPageLocators.EMAIL)
+        pochya.send_keys(f"{email}")
+        parol=self.browser.find_element(*LoginPageLocators.PASSWORD)
+        parol.send_keys(f"{password}")
+        parol_povtor=self.browser.find_element(*LoginPageLocators.CONFIRM_PASSWORD)
+        parol_povtor.send_keys(f"{password}")
+        knopka=self.browser.find_element(*LoginPageLocators.BUTTON)
+        knopka.click()
+        
+        
